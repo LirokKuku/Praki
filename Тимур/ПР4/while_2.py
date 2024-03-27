212c8805fd8f6bcd39a47dabc932bf159786402d
4utep <4utep12@gmail.com>
2024-03-27 13:01:30 +0300

    27.03.2024

--- Тимур/ПР4/while_2.py
+++ Тимур/ПР4/while_2.py
@@ -1,12 +1,20 @@
+import sys
+
 k = 0
 a = 1
+summ = 0
+maxx = -sys.maxsize - 1
+minn = sys.maxsize
 
 while a != 0:
-    a = float(input())
-    maxx = max(maxx, a)
-    minn = min(minn, a)
-    summ = summ + a
-    k += 1
+        a = float(input())
+        if a == 0:
+            break
+        else:
+            maxx = max(maxx, a)
+            minn = min(minn, a)
+            summ = summ + a
+            k += 1
 print("Количество чисел:", k)
 print("Максимальное число:", maxx)
 print("Минимальное число:", minn)
