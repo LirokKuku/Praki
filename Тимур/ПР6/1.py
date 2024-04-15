e5f7ecd66d0ff71a0d894f907ed784b2f907013a
Lirok <tumypak@gmail.com>
2024-04-16 00:27:41 +0300

    1

--- Тимур/ПР6/1.py
+++ Тимур/ПР6/1.py
@@ -1,12 +1,31 @@
-from tkinter import *
-from tkinter import ttk
+f = open('number.txt', 'r') # я решил создать файл с 30 значениями
+fp = f.readlines()
+n = 5
+m = 6
 
-root = Tk()
-root.title("METANIT.COM")
-root.geometry("250x200")
 
-languages = ["Python", "C#", "Java", "JavaScript"]
-combobox = ttk.Combobox(values=languages)
-combobox.pack(anchor=NW, padx=6, pady=6)
+list_n = list() # список Big (который снаружи)
+list_m = list() # список Small (который внутри)
 
-root.mainloop()
+# тупо ходим по массиву
+o = 0 - 6
+h = 6 - 6
+
+# создаём матрицу
+for i in range(n):
+    o += m
+    h += m
+    print(o, h)
+    for j in range(o, h):
+        list_m.append(int(fp[j]))
+    list_n.append(list_m)
+    list_m = list()
+
+nulli = []
+for i in range(n):
+    if 0 in list_n[i]:
+        nulli.append(i + 1)
+        if list_n.count(list_n[i]) != 1:
+            print("Тута есть дубликаты")
+        else:
+            p
