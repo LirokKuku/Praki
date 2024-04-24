ceedf80f44294e74a77cea0d07f00263b5618981
4utep <4utep12@gmail.com>
2024-04-24 13:23:39 +0300

    24.04.2024

--- Тимур/ПР6/1.py
+++ Тимур/ПР6/1.py
@@ -3,16 +3,12 @@ fp = f.readlines()
 n = 5
 m = 6
 
+list_n1 = list()
+list_m1 = list()
 
-list_n1 = list() # список Big (который снаружи)
-list_m1 = list() # список Small (который внутри)
-
-# тупо ходим по массиву
 o = 0 - 6
 h = 6 - 6
 
-
-# создаём матрицу
 for i in range(n):
     o += m
     h += m
@@ -21,43 +17,56 @@ for i in range(n):
     list_n1.append(list_m1)
     list_m1 = list()
 
-# условние 1 и 2
 nulli = []
 for i in range(n):
     if 0 in list_n1[i]:
         nulli.append(i + 1)
         if list_n1.count(list_n1[i]) != 1:
-            print("Тута есть дубликаты")
+            print("Тута есть нули")
         else:
-            print("Тута нет дубликатов")
+            print("Тута нет нулей")
 print(*nulli)
 
-# это для условния 3
 list_n2 = list()
 
-#транспонированная матрица
+k = 0
+for i in range(len(list_n1)):
+    if list_n1.count(list_n1[i]) > 1:
+         k += 1
+print('Количество одинаковых строк:', int(k/2))
+
 for i in range(m):
     list_m2 = list()
     for j in range(n):
         list_m2.append(list_n1[j][i])
     list_n2.append(list_m2)
 
-# удаление строк
 n_1 = int(input())
 n_2 = int(input())
-# for i in range(n):
-#     for j in range(m):
-#         if j + 1 == n_2:
-#             list_n1[i].remove(list_n1[i][j])
-# list_n1.remove(list_n1[n_1 - 1])
+command = str(input())
 
-# вставка строки с столбцы
-n_1 = int(input())
-n_2 = int(input())
-l_0 = [0 for x in range(m)]
-for i in range(n + 1):
-    if i + 1 == n_1:
-        list_n1.insert(0, l_0)
-    for j in range(m):
-        if j + 1 == n_2:
-            list_n1[i].insert(j, 0)
+def one(x_1, x_2):
+    for i in range(n):
+        for j in range(m):
+            if j + 1 == x_2:
+                list_n1[i].remove(list_n1[i][j])
+    list_n1.remove(list_n1[x_1 - 1])
+    return list_n1
+def two(x_1, x_2):
+    l_0 = [0 for x in range(m)]
+    for i in range(n + 1):
+        if i + 1 == x_1:
+            list_n1.insert(0, l_0)
+        for j in range(m):
+            if j + 1 == x_2:
+                list_n1[i].insert(j, 0)
+    return list_n1
+
+if command == '1':
+    p = one(n_1, n_2)
+    for i in list_n1:
+        print(i)
+elif command == "2":
+    p = two(n_1, n_2)
+    for i in list_n1:
+        print(i)
